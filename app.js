6bfa5e3a2c0612bbd94308733c9beff737b27c84
Farhan Ahmed <153209469+Connect4farhan@users.noreply.github.com>
2024-02-05 16:16:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+
+// Q-1
+// var a=+prompt("Enter a Value for a : ");
+// console.log("The Value of a is : " + a);
+// console.log("The Value of ++a is : " + (++a));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of a++ is : " + (a++));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of --a is : " + (--a));
+// console.log("Now the Value of a is : " + a);
+// console.log("The Value of a-- is : " + (a--));
+// console.log("Now the Value of a is : " + a);
+
+// Q-2
+// var a = 2, b = 1;
+// var prefix = --a;
+// console.log("the Value of --a is: " + prefix);
+// var prefix2 = --a - --b;
+// console.log("the Value of --a - --b is: " + prefix2);
+// var prefix3 = (--a - --b) + ++b;
+// console.log("the Value of --a - --b + ++b is:" + prefix3);
+// var prefix4 = (--a - --b) + (++b + b--);
+// console.log("the Value of --a - --b + ++b + b-- is:" + prefix4);
+// var result = --a - --b + ++b + b--;
+// console.log("The result is " + result);
+
+// Q-3
+// var name = prompt("Enter Your Good Name : ");
+// console.log("Welcome "+ name +" to our website ");
+
+// Q-4
+// var num = +prompt("Enter one number to see the table of his choice");
+
+// if (num != 0){
+//     var i;
+//     for (i=1 ; i<=10 ;i++){
+//         console.log(num +" x " + i + " = " + num * i);
+//     }  
+// }
+// else if (num == 0) {
+//     var i , a=5;
+//     for (i=1 ; i<=10 ;i++){
+//         console.log(a +" x " + i + " = " + a * i);
+//     }  
+// }
+
+// Q-5
+// var sub1 = prompt("Enter subject 1 name : ");
+// var sub2 = prompt("Enter subject 2 name : ");
+// var sub3 = prompt("Enter subject 3 name : ");
+
+// var tm = 100;
+
+// var obtainMarks1 = +prompt("Enter subject 1 marks : ");
+
+// var obtainMarks2 = +prompt("Enter subject 2 marks : ");
+// var obtainMarks3 = +prompt("Enter subject 3 marks : ");
+
+
+// var totalMarks = obtainMarks1 + obtainMarks2 + obtainMarks3;
+// var totalSubjects = 3;
+// var percentage = (totalMarks / (tm * totalSubjects)) * 100;
+// var resultMessage = (
+//     "Subject   Total Marks  Obtained Marks\n" +
+//     sub1 + "                 " + tm + "                        " + obtainMarks1 + "\n" +
+//     sub2 + "                 " + tm + "                        " + obtainMarks2 + "\n" +
+//     sub3 + "                 " + tm + "                         " + obtainMarks3 + "\n\n" +
+//     "Total                    " + totalMarks + "\n" +
+//     "Percentage       " + percentage + "%");
+// alert(resultMessage);
+// console.log(resultMessage);
+
+
+
